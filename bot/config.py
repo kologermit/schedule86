9510b2912dba930d86cdac1482a65ef26b031512
kologermit <kologermit@gmail.com>
2024-07-09 16:51:59 +0400

    README добавил настройку бота

--- bot/config.py
+++ bot/config.py
@@ -1,4 +1,3 @@
-# TOKEN = '5469643644:AAGswk4CVHLOTVx6ZwsVglcKcxkLQeMd030'
 TOKEN = "1375982367:AAE2irmoKbNdfsa7ZGoY2qP4d30iHeatTEE"
 mysql = {
     "user": "root",
@@ -6,9 +5,7 @@ mysql = {
     "host": "mysql",
     "database": "schedule86"
 }
-weather_api_key = "5fbabd1a278d9b5056aafd129a588954"
 API_KEY = "5fbabd1a278d9b5056aafd129a588954"
-zaglushka = "На данный момент бот не работает!"
 
 try:
     from env import *
